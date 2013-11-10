8a3a8408d5207354185fbfe30c5914536c0f9ad6
ShiloOlga <shilo.olga@gmail.com>
2013-11-10 18:21:46 +0300

    Empty string test (compilation failed)

---
+++ TestApp/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+namespace TestApp
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void TestAddEmptyString()
+        {
+            var sut = new Calc();
+            var sum = calc.Add();
+            Assert.AreEqual(0, sum);
+        }
+    }
+}
