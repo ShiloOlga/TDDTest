3b13a92eeb80cda79197cda59d2260d35167b0f7
ShiloOlga <shilo.olga@gmail.com>
2013-11-10 18:56:11 +0300

    Removed duplicated code

--- TestApp/Tests.cs
+++ TestApp/Tests.cs
@@ -10,48 +10,6 @@ namespace TestApp
     {
         private Calc sut = new Calc();
 
-        [Test]
-        public void TestAddEmptyString()
-        {
-            var sum = sut.Add();
-            Assert.AreEqual(0, sum);
-        }
-
-        [Test]
-        public void TestAddOne()
-        {
-            var sum = sut.Add("1");
-            Assert.AreEqual(1, sum);
-        }
-
-        [Test]
-        public void TestAddTwo()
-        {
-            var sum = sut.Add("2");
-            Assert.AreEqual(2, sum);
-        }
-
-        [Test]
-        public void TestAddZeroZero()
-        {
-            var sum = sut.Add("0,0");
-            Assert.AreEqual(0, sum);
-        }
-
-        [Test]
-        public void TestAddZeroOne()
-        {
-            var sum = sut.Add("0,1");
-            Assert.AreEqual(1, sum);
-        }
-
-        [Test]
-        public void TestAddOneZero()
-        {
-            var sum = sut.Add("1,0");
-            Assert.AreEqual(1, sum);
-        }
-
         [TestCase("", 0)]
         [TestCase("0", 0)]
         [TestCase("1", 1)]
