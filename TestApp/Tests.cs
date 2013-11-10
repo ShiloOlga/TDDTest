fcf0f7406bb0b07eaf4b5c50ab0fb541654e705a
ShiloOlga <shilo.olga@gmail.com>
2013-11-10 18:55:28 +0300

    Fixed error in common test. Test all available inputs with TestCase

--- TestApp/Tests.cs
+++ TestApp/Tests.cs
@@ -52,11 +52,21 @@ namespace TestApp
             Assert.AreEqual(1, sum);
         }
 
-        [TestCase ("", 0)]
+        [TestCase("", 0)]
+        [TestCase("0", 0)]
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("0,1", 1)]
+        [TestCase("1,0", 1)]
+        [TestCase("0,2", 2)]
+        [TestCase("2,0", 2)]
+        [TestCase("1,2", 3)]
+        [TestCase("2,1", 3)]
+        [TestCase("2,2", 4)]
         public void TestAdd(string input, decimal result)
         {
-            var sum = sut.Add("1,0");
-            Assert.AreEqual(1, sum);
+            var sum = sut.Add(input);
+            Assert.AreEqual(result, sum);
         }
     }
 }
